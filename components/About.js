15f9acaf6fb57d06063dfb7517dd79b77478742f
divy <divy14566@gmail.com>
2025-06-02 07:00:10 +0530

    initial commit

--- components/About.js
+++ components/About.js
@@ -3,12 +3,12 @@ import React from 'react'
 const About = () => {
 return (
 <>
-<section id='about' className='sansita-regular mt-[10vw] sm:mt-[8vw] m-10 mx-[1.5vmax] min-h-screen'>
+<section id='about' className='sansita-regular mt-[10vw] sm:mt-[8vw] mt-10 mx-10 mx-[1.5vmax]'>
       <div>
         <h1 className='text-[10vmin] font-bold mb-0 p-0 mx-[1.5vmax]'>About</h1>
         <div className='mt-0 mx-[1.5vmax] mb-3.5 w-[13vmin] rounded-full h-[3vmin] bg-gradient-to-r from-violet-800 to-[#d10da1]'></div>
 
-        <div className='my-[4.3vw] mx-[1.5vmax] text-[2.5vmax] text-slate-300 bg-[#0c0011]'>
+        <div className='mt-[4.3vw] mx-[1.5vmax] text-[2.5vmax] text-slate-300 pb-5'>
           <p>
   <span className='text-slate-100 font-extrabold'>Hi, I'm Divy</span> — a passionate Web Developer who loves turning ideas into interactive, scalable digital experiences. With a strong foundation in front-end technologies like <span className="text-slate-100 font-bold">HTML, CSS, JavaScript, and Tailwind CSS</span>, I specialize in crafting clean, responsive, and user-focused interfaces.
 </p>
